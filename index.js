9fde34234f900cf88aba87ec4941d34b4baaab6d
Devam <satasiyadevam@gmail.com>
2023-12-26 09:39:42 +0530

    no change

--- index.js
+++ index.js
@@ -142,13 +142,15 @@ function setdetails(con ,key ,content, title) {
 function generateHtml(title, content) {
   const m = d.getMonth() + 1;
   const date = "      " + d.getDate() + "/" + m + "/" + d.getFullYear();
-  let html = `
-    <!DOCTYPE html>
+  let html = 
+  `<!DOCTYPE html>
     <html lang="en">
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>My Blog</title>
+      <link rel="shortcut icon" href="/images/logo.png" type="image/x-icon">
+      
       <style>
       body {
         font-family: 'Arial', sans-serif;
